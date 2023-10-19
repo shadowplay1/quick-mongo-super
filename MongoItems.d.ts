2f25b0fa037c9aa6fffd33b9375d84a03ba59908
shadowplay1 <shadowowplay483@gmail.com>
2023-10-20 02:40:46 +0300

    feat(rewrite): v2.0.0 init

--- MongoItems.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './typings/src/classes/DatabaseError'
-export * from './typings/src/classes/Emitter'
-export * from './typings/src/classes/Utils'
-
-export * from './typings/src/interfaces/QuickMongo'
-
-export import Mongo = require('./typings/src/index')
---
+++ src/lib/utils/Emitter.ts
@@ -0,0 +1,40 @@
+import { EventEmitter } from 'events'
+const emitter = new EventEmitter()
+
+export class Emitter<E extends Record<string, any[]>> {
+
+    /**
+     * Listens to the event.
+     * @param {string} event Event name.
+     * @param {Function} listener Listener function.
+     */
+    public on<K extends Exclude<keyof E, number>>(event: K, listener: (...args: E[K]) => any): Emitter<E> {
+        emitter.on(event, listener)
+        return this
+    }
+
+    /**
+     * Listens to the event only for once.
+     * @param {string} event Event name.
+     * @param {Function} listener Listener function.
+     */
+    public once<K extends Exclude<keyof E, number>>(event: K, listener: (...args: E[K]) => any): Emitter<E> {
+        emitter.once(event, listener)
+        return this
+    }
+
+    /**
+     * Emits the event.
+     * @param {string} event Event name.
+     * @param {any[]} args Listener arguments.
+     * @returns {boolean} `true` if the event had listeners, `false` otherwise.
+     */
+    public emit<K extends Exclude<keyof E, number>>(event: K, ...args: E[K]): boolean {
+        return emitter.emit(event, ...args)
+    }
+}
+
+const a = new Emitter<{test: [param1: string, param2: number]}>()
+a.on('test', (x, y) => {
+    y
+})
