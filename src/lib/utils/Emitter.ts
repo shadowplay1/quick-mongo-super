5ba40adb609c99933eb54604475c73b7e36be830
shadowplay1 <shadowowplay483@gmail.com>
2023-10-24 00:57:32 +0300

    docs: added full jsdoc for all the methods, properties, types and classes

--- src/lib/utils/Emitter.ts
+++ src/lib/utils/Emitter.ts
@@ -33,4 +33,3 @@ export class Emitter<E extends Record<string, any>> {
         return emitter.emit(event, ...args as any[])
     }
 }
-
--- src/structures/errors.ts
+++ src/structures/errors.ts
@@ -1,17 +1,27 @@
+import { InferFromRestOrArray } from '../types/utils'
+import { typeOf } from '../lib/utils/functions/typeOf.function'
+
 export const errors = {
-    NOT_CONNECTED: 'MongoDB connection is not established.',
+    NOT_CONNECTED: 'No connection to MongoDB.',
     UNKNOWN_ERROR: 'Unknown error.',
     REQUIRED_PARAMETER_MISSING: '\'{1}\' parameter is required but is missing.',
     INVALID_TYPE: '\'{1}\' must be a type of {2}. Received type: {3}',
+    ONE_OR_MORE_TYPES_INVALID: 'All the specified elements from the array in \'{1}\' parameter ' +
+        'must be a type of {2}. Received types array: {3}',
     INVALID_TARGET: 'The target must be a type of {1}. Received type: {2}.'
 }
 
 export interface IErrorParams extends Record<keyof typeof errors, any[]> {
     NOT_CONNECTED: []
     UNKNOWN_ERROR: []
-    INVALID_TYPE: [paramName: string, targetType: TargetParamType, receivedType: string]
     REQUIRED_PARAMETER_MISSING: [missingParam: string]
+    INVALID_TYPE: [paramName: string, targetType: TargetParamType, receivedType: string]
+    ONE_OR_MORE_TYPES_INVALID: [paramName: string, targetType: TargetParamType, receivedTypesArray: string]
     INVALID_TARGET: [targetType: TargetParamType, receivedType: string]
+}
+
+export const createTypesArray = <T>(...elements: InferFromRestOrArray<T>): string => {
+    return `[${elements.map(element => typeOf(element))}]`
 }
 
 export type TargetParamType = 'string' | 'number' | 'array'
