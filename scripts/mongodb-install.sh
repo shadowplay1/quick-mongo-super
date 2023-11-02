9091ba613155fdd39ba2f0f485cb30d03cfbf023
shadowplay1 <shadowplay483@gmail.com>
2023-11-02 12:16:36 +0300

    test: added deletion check test

--- scripts/mongodb-install.sh
+++ scripts/mongodb-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install gnupg curl
+apt-get install -y gnupg curl
 
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
@@ -8,5 +8,5 @@ curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-get update
+apt-get install -y mongodb-org
