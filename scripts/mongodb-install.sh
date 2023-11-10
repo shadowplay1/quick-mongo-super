1517fcc901d65ed788b99f2d8e2b33068f8f1472
shadowplay1 <shadowplay483@gmail.com>
2023-11-10 11:30:40 +0300

    fix(mongodb-install): running apt-get commands as root

--- scripts/mongodb-install.sh
+++ scripts/mongodb-install.sh
@@ -8,5 +8,5 @@ curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-apt-get update
-apt-get install -y mongodb-org
+sudo apt-get update
+sudo apt-get install -y mongodb-org
